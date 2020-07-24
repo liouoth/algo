4aa1a76a3078faea4c8d0b507e58e260bce1507d
liouoth <mwqyykl@gmail.com>
2020-07-24 17:45:44 +0800

    mwq write stack code

--- src/栈/Stack.java
+++ src/栈/Stack.java
@@ -1,5 +1,6 @@
 package 栈;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Stack<T> {
@@ -13,7 +14,36 @@ public class Stack<T> {
         top = 0;
     }
 
-    public String pop(){
-        return null;
+    public T pop(){
+        if (container==null||top==0){
+            return null;
+        }
+        top--;
+        return container[top-1];
     }
+
+    public void push(T newOne){
+        if (top==length){
+            container = Arrays.copyOf(container,2*length);
+            length = 2*length;
+        }
+        container[top] = newOne;
+        top++;
+    }
+
+    public static void main(String[] args) {
+        Stack<String> test = new Stack<>(2);
+        test.push("1234");
+        test.push("1");
+        System.out.println(test.length);
+        test.push("1");
+        System.out.println(test.length);
+        test.push("1");
+        System.out.println(test.length);
+        System.out.println(test.top);
+        System.out.println(Arrays.toString(test.container));
+        test.pop();
+        System.out.println(Arrays.toString(test.container)+"top:"+test.top);
+    }
+
 }
